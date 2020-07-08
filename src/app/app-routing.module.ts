42eb1b3df1c2f07ad79bfab9da0ef899342000a3
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-08 22:18:50 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ModalComponent } from './modal/modal.component';
+import { TypeAheadComponent } from './type-ahead/type-ahead.component';
+import { PopOverComponent } from './pop-over/pop-over.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ /* {
+    path:"",
+    component:NavigationComponent
+  },*/
+  {
+    path:"accordion",
+    component:AccordionComponent
+  },
+  {
+    path:"carousel",
+    component:CarouselComponent
+  },
+  {
+    path:"modal",
+    component:ModalComponent
+  },
+  {
+    path:"type-ahead",
+    component:TypeAheadComponent
+  },
+  {
+    path:"pop-over",
+    component:PopOverComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ModalComponent } from './modal/modal.component';
+import { TypeAheadComponent } from './type-ahead/type-ahead.component';
+import { PopOverComponent } from './pop-over/pop-over.component';
+
+//@NgModule({
+  //imports: [NgbModule],
+//})
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AccordionComponent,
+    CarouselComponent,
+    ModalComponent,
+    TypeAheadComponent,
+    PopOverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
